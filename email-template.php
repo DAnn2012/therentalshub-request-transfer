eb8be3136d21e5025a2574cad6a33cf31decb65e
Steve Armenakis <support@shopie.gr>
2026-02-16 13:03:01 +0200

    new date format

--- email-template.php
+++ email-template.php
@@ -1,5 +1,5 @@
 <div>
-    <p>Dear <?=htmlentities($vars->fname, ENT_QUOTES);?>,<br>thank you for your enquiry. We will check our availability and contact you as soon as possible with booking details.</p>
+    <p>Dear <?=htmlspecialchars($vars->fname, ENT_QUOTES);?>,<br>thank you for your enquiry. We will check our availability and contact you as soon as possible with booking details.</p>
     <p>Following are your transfer request details.</p>
     <div style="margin-top:30px">
         <table cellpadding="5" style="text-align:left;border-spacing:10px;border:1px #ccc solid">
@@ -8,15 +8,15 @@
             </tr>
             <tr>
                 <th>Transfer date/time</th>
-                <td><?=htmlentities($vars->arvd, ENT_QUOTES);?> at <?=htmlentities($vars->arvt, ENT_QUOTES);?></td>
+                <td><?=htmlspecialchars(date('d/m/Y', strtotime($vars->arvd)), ENT_QUOTES);?> at <?=htmlspecialchars($vars->arvt, ENT_QUOTES);?></td>
             </tr>
             <tr>
                 <th>Pick-up from</th>
-                <td><?=htmlentities($vars->arvfl, ENT_QUOTES);?></td>
+                <td><?=htmlspecialchars($vars->arvfl, ENT_QUOTES);?></td>
             </tr>
             <tr>
                 <th>Destination</th>
-                <td><?=htmlentities($vars->arvtl, ENT_QUOTES);?></td>
+                <td><?=htmlspecialchars($vars->arvtl, ENT_QUOTES);?></td>
             </tr>
             <tr>
                 <th>Passengers</th>
@@ -24,7 +24,7 @@
             </tr>
             <tr>
                 <th>Flight/ship info</th>
-                <td><?=htmlentities($vars->arvf, ENT_QUOTES);?></td>
+                <td><?=htmlspecialchars($vars->arvf, ENT_QUOTES);?></td>
             </tr>
             <?php if ($vars->type == 2): ?>
             <tr>
@@ -32,15 +32,15 @@
             </tr>
             <tr>
                 <th>Transfer date/time</th>
-                <td><?=htmlentities($vars->dptd, ENT_QUOTES);?> at <?=htmlentities($vars->dptt, ENT_QUOTES);?></td>
+                <td><?=htmlspecialchars(date('d/m/Y', strtotime($vars->dptd)), ENT_QUOTES);?> at <?=htmlspecialchars($vars->dptt, ENT_QUOTES);?></td>
             </tr>
             <tr>
                 <th>Pick-up from</th>
-                <td><?=htmlentities($vars->dptfl, ENT_QUOTES);?></td>
+                <td><?=htmlspecialchars($vars->dptfl, ENT_QUOTES);?></td>
             </tr>
             <tr>
                 <th>Destination</th>
-                <td><?=htmlentities($vars->dpttl, ENT_QUOTES);?></td>
+                <td><?=htmlspecialchars($vars->dpttl, ENT_QUOTES);?></td>
             </tr>
             <tr>
                 <th>Passengers</th>
@@ -48,7 +48,7 @@
             </tr>
             <tr>
                 <th>Flight/ship info</th>
-                <td><?=htmlentities($vars->arvf, ENT_QUOTES);?></td>
+                <td><?=htmlspecialchars($vars->arvf, ENT_QUOTES);?></td>
             </tr>
             <?php endif; ?>
             <?php if ($vars->notes != ''): ?>
@@ -57,7 +57,7 @@
             </tr>
             <tr>
                 <th>Notes</th>
-                <td><?=htmlentities($vars->notes, ENT_QUOTES);?></td>
+                <td><?=htmlspecialchars($vars->notes, ENT_QUOTES);?></td>
             </tr>
             <?php endif; ?>
             <tr>
@@ -65,15 +65,15 @@
             </tr>
             <tr>
                 <th>Customer</th>
-                <td><?=htmlentities($vars->fname, ENT_QUOTES);?> <?=htmlentities($vars->lname, ENT_QUOTES);?></td>
+                <td><?=htmlspecialchars($vars->fname, ENT_QUOTES);?> <?=htmlspecialchars($vars->lname, ENT_QUOTES);?></td>
             </tr>
             <tr>
                 <th>Email</th>
-                <td><?=htmlentities($vars->email, ENT_QUOTES);?></td>
+                <td><?=htmlspecialchars($vars->email, ENT_QUOTES);?></td>
             </tr>
             <tr>
                 <th>Phone</th>
-                <td><?=htmlentities($vars->phone, ENT_QUOTES);?></td>
+                <td><?=htmlspecialchars($vars->phone, ENT_QUOTES);?></td>
             </tr>
         </table>
     </div>
